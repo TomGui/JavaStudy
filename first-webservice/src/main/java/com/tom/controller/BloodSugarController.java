6d47823ead44346c6d0e34a857254aaf81dee5ef
TomGui <guiyueping@126.com>
2017-07-09 22:29:06 +0800

    参数校验器

--- first-webservice/src/main/java/com/tom/controller/BloodSugarController.java
+++ first-webservice/src/main/java/com/tom/controller/BloodSugarController.java
@@ -4,6 +4,9 @@ import com.tom.dto.AddBloodSugarInput;
 import com.tom.entity.BloodSugar;
 import com.tom.service.IBloodSugarService;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.sql.Timestamp;
+import java.util.List;
 
 @Controller
 @RequestMapping("/bloodSugar")
@@ -20,7 +24,17 @@ public class BloodSugarController {
 
     @ResponseBody
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public int Add(@RequestBody AddBloodSugarInput addBloodSugarInput) throws Exception {
+    public String Add(@RequestBody @Validated AddBloodSugarInput addBloodSugarInput, BindingResult bindingResult)
+            throws Exception {
+        if (bindingResult.hasErrors()) {
+            String errorMsg = "";
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            for (ObjectError objectError : allErrors) {
+                errorMsg += objectError.getDefaultMessage() + ",";
+            }
+            return errorMsg;
+        }
+
         BloodSugar bloodSugar = new BloodSugar();
         bloodSugar.setId(System.currentTimeMillis());
         bloodSugar.setPatientId(addBloodSugarInput.getPatientId());
@@ -30,6 +44,9 @@ public class BloodSugarController {
         bloodSugar.setDataSource(addBloodSugarInput.getDataSource());
         bloodSugar.setAddDoctorId(addBloodSugarInput.getAddDoctorId());
         bloodSugar.setAddTime(new Timestamp(System.currentTimeMillis()));
-        return bloodSugarService.add(bloodSugar);
+        if (bloodSugarService.add(bloodSugar) > 0) {
+            return "添加成功";
+        }
+        return "添加失败";
     }
 }
--- first-webservice/src/main/java/com/tom/dto/AddBloodSugarInput.java
+++ first-webservice/src/main/java/com/tom/dto/AddBloodSugarInput.java
@@ -1,5 +1,8 @@
 package com.tom.dto;
 
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public class AddBloodSugarInput {
@@ -7,12 +10,15 @@ public class AddBloodSugarInput {
     private long patientId;
 
     // 血糖值
+    @NotNull(message = "{addBloodSugarInput.value.isNull}")
     private BigDecimal value;
 
     // 测量类型（1：空腹血糖，2：饭后两小时血糖，3：随机血糖）
+    @Range(min = 1, max = 3, message = "{addBloodSugarInput.measureType.error}")
     private int measureType;
 
     // 数据来源（1：微信录入，2：后台录入，3：医护端录入）
+    @Range(min = 1, max = 3, message = "{addBloodSugarInput.dataSource.error}")
     private int dataSource;
 
     // 录入医生Id
